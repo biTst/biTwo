063b8a2cee12b9b2041e65c524affc7592816c4d
biTst <papikcar@gmail.com>
2017-08-05 15:47:04 +0300

    TradeTickData added

--- src/bi/two/chart/TickVolumeData.java
+++ src/bi/two/chart/TickVolumeData.java
@@ -1,7 +1,7 @@
 package bi.two.chart;
 
 public class TickVolumeData extends TickData {
-    private float m_volume;
+    float m_volume;
 
     public TickVolumeData(long timestamp, float price, float volume) {
         super(timestamp, price);
---
+++ src/bi/two/chart/TradeTickData.java
@@ -0,0 +1,26 @@
+package bi.two.chart;
+
+public class TradeTickData extends TickVolumeData {
+    private final long m_tradeId;
+
+    public TradeTickData(long tradeId, long timestamp, float price, float volume) {
+        super(timestamp, price, volume);
+        m_tradeId = tradeId;
+    }
+
+    @Override public int hashCode() {
+        return (int)m_tradeId;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TradeTickData that = (TradeTickData) o;
+        return m_tradeId == that.m_tradeId;
+    }
+
+    @Override public String toString() {
+        return "TradeTickData[tradeId=" + m_tradeId + "; time=" + m_timestamp + "; price=" + m_price + "; volume=" + m_volume + "]";
+    }
+}
