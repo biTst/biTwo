3bfa1e4794158c394beeb25bd53e0a8944284de0
biTst <papikcar@gmail.com>
2017-10-31 02:20:45 +0200

    MinMaxSpread calc

--- src/bi/two/algo/impl/Mmar.java
+++ src/bi/two/algo/impl/Mmar.java
@@ -8,6 +8,7 @@ import bi.two.calc.BarsTEMA;
 import bi.two.calc.TicksRegressor;
 import bi.two.chart.*;
 import bi.two.opt.Vary;
+import bi.two.ts.BaseTimesSeriesData;
 import bi.two.ts.ITimesSeriesData;
 import bi.two.ts.TimesSeriesData;
 import bi.two.util.MapConfig;
@@ -22,8 +23,9 @@ public class Mmar extends BaseAlgo {
     private final float m_count;
     private final float m_smooth;
     private final List<BarsTEMA> m_emas = new ArrayList<>();
-    private final List<TicksRegressor> m_emasSm = new ArrayList<>(); //smooched
+    private final List<BaseTimesSeriesData> m_emasSm = new ArrayList<>(); //smooched
     private final TicksRegressor m_firstEmaSm;
+//    private final MinMaxSpread m_minMaxSpread;
     private TickData m_tickData; // latest tick
 
     public Mmar(MapConfig config, ITimesSeriesData tsd) {
@@ -35,6 +37,7 @@ public class Mmar extends BaseAlgo {
         m_count = config.getNumber(Vary.count).floatValue();
         m_smooth = config.getNumber(Vary.smooth).floatValue();
 
+        List<ITimesSeriesData> iEmasSm = new ArrayList<>(); // as list of ITimesSeriesData
         long smoothLen = (long) (m_smooth * barSize);
         TicksRegressor first = null;
         float length = m_start;
@@ -44,6 +47,7 @@ public class Mmar extends BaseAlgo {
             m_emas.add(ema);
             TicksRegressor emaSm = new TicksRegressor(ema, smoothLen);
             m_emasSm.add(emaSm);
+            iEmasSm.add(emaSm);
 
             length += m_step;
             if (i == 0) {
@@ -59,6 +63,8 @@ public class Mmar extends BaseAlgo {
 
         m_firstEmaSm = first;
 
+//        m_minMaxSpread = new MinMaxSpread(iEmasSm, tsd);
+
         setParent(m_firstEmaSm);
     }
 
@@ -70,7 +76,7 @@ public class Mmar extends BaseAlgo {
         double firstValue = latestTick.getClosePrice();
         double minValue = Double.POSITIVE_INFINITY;
         double maxValue = Double.NEGATIVE_INFINITY;
-        for (TicksRegressor emaSm : m_emasSm) {
+        for (BaseTimesSeriesData emaSm : m_emasSm) {
             latestTick = emaSm.getLatestTick();
             if (latestTick == null) {
                 return null; // not yet defined/filled
@@ -120,7 +126,7 @@ public class Mmar extends BaseAlgo {
                 Color color = (i == 0) ? Color.BLUE : (i == size - 1) ? Colors.alpha(Color.GRAY, 100) : emaColor;
                 addChart(chartData, ema.getJoinNonChangedTs(), topLayers, "ema" + i, color, TickPainter.LINE);
 
-                TicksRegressor emaSm = m_emasSm.get(i);
+                BaseTimesSeriesData emaSm = m_emasSm.get(i);
                 Color colorSm = (i == 0) ? Color.PINK : (i == size - 1) ? Colors.alpha(Color.GRAY, 100) : emaSmColor;
                 addChart(chartData, emaSm.getJoinNonChangedTs(), topLayers, "emaSm" + i, colorSm, TickPainter.LINE);
             }
@@ -152,6 +158,65 @@ public class Mmar extends BaseAlgo {
 
             List<ChartAreaLayerSettings> gainLayers = gain.getLayers();
             addChart(chartData, firstWatcher.getGainTs(), gainLayers, "gain", Color.blue, TickPainter.LINE);
+        }
+    }
+
+
+    //----------------------------------------------------------
+    public static class MinMaxSpread extends BaseTimesSeriesData<ITickData> {
+        private final List<ITimesSeriesData> m_tss;
+        private final ITimesSeriesData m_first;
+        private boolean m_dirty;
+        private ITickData m_tick;
+        private float m_spread;
+        private float m_min;
+        private float m_max;
+
+        MinMaxSpread(List<ITimesSeriesData> tss, ITimesSeriesData baseDs) {
+            super(null);
+            m_tss = tss;
+            for (ITimesSeriesData<ITickData> next : tss) {
+                next.getActive().addListener(this);
+            }
+            m_first = tss.get(0);
+            setParent(baseDs); // subscribe to list first - will be called onChanged() and set as dirty ONLY
+        }
+
+        @Override public void onChanged(ITimesSeriesData ts, boolean changed) {
+            if (ts == getParent()) {
+                super.onChanged(ts, changed); // forward onChanged() only for parent ds. other should only reset changed state
+            } else {
+                if (changed) {
+                    m_dirty = true;
+                }
+            }
+        }
+
+        @Override public ITickData getLatestTick() {
+            if (m_dirty) {
+                float min = Float.POSITIVE_INFINITY;
+                float max = Float.NEGATIVE_INFINITY;
+                boolean allDone = true;
+                for (ITimesSeriesData<ITickData> next : m_tss) {
+                    ITickData lastTick = next.getLatestTick();
+                    if (lastTick != null) {
+                        float value = lastTick.getClosePrice();
+                        min = Math.min(min, value);
+                        max = Math.max(max, value);
+                    } else {
+                        allDone = false;
+                        break; // not ready yet
+                    }
+                }
+                if (allDone) {
+                    m_min = min;
+                    m_max = max;
+                    m_spread = max - min;
+                    m_dirty = false;
+                    m_tick = new TickData(m_first.getLatestTick().getTimestamp(), m_spread);
+                }
+            }
+            return m_tick;
         }
     }
 }
