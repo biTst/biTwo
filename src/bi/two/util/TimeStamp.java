4a796681e857add155614f41a85341713b441837
papikcar <papikcar@gmail.com>
2018-01-23 04:24:33 +0200

    start/reset

--- src/bi/two/util/TimeStamp.java
+++ src/bi/two/util/TimeStamp.java
@@ -1,17 +1,35 @@
 package bi.two.util;
 
 public class TimeStamp {
-    private final long m_millis;
+    private long m_millis;
 
     public TimeStamp() {
-        m_millis = System.currentTimeMillis();
+        this(System.currentTimeMillis());
+    }
+
+    public TimeStamp(long millis) {
+        m_millis = millis;
+    }
+
+    public void reset() {
+        m_millis = 0;
     }
 
     public long getPassedMillis() {
-        return System.currentTimeMillis() - m_millis;
+        return (m_millis == 0) ? 0 : System.currentTimeMillis() - m_millis;
     }
 
     public String getPassed() {
         return Utils.millisToYDHMSStr(getPassedMillis());
+    }
+
+    public void startIfNeeded() {
+        if (m_millis == 0) {
+            m_millis = System.currentTimeMillis();
+        }
+    }
+
+    public boolean isStarted() {
+        return (m_millis != 0);
     }
 }
