8803bc41a18100d84a06aee261dca53afe753af1
papikcar <papikcar@gmail.com>
2018-01-24 15:00:21 +0200

    format8

--- src/bi/two/tre/OrderWatcher.java
+++ src/bi/two/tre/OrderWatcher.java
@@ -19,9 +19,9 @@ class OrderWatcher {
     public State m_state = State.none;
     public final ExchPairData m_exchPairData;
     private OrderData m_orderData;
-    public TimeStamp m_outOfTopSpreadStamp = new TimeStamp(0); // time when order becomes ot of spread
-    public TimeStamp m_onTopSpreadAloneStamp = new TimeStamp(0);
-    public TimeStamp m_onTopSpreadWithOthersStamp = new TimeStamp(0);
+    public TimeStamp m_outOfTopSpreadStamp = new TimeStamp(0); // time when order becomes out of spread
+    public TimeStamp m_onTopSpreadAloneStamp = new TimeStamp(0); // time when order appears on spread alone
+    public TimeStamp m_onTopSpreadWithOthersStamp = new TimeStamp(0); // time when order appears on spread with other
     private OrderBook.Spread m_lastTopSpread;
     private OrderData.IOrderListener m_orderListener = new OrderData.IOrderListener() {
         @Override public void onUpdated(OrderData orderData) {
@@ -67,17 +67,17 @@ class OrderWatcher {
         }
     }
 
-    boolean onSecTimer() {
+    boolean onTimer() {
         if (m_state == State.done) {
             return true;
         }
         OrderStatus orderStatus = m_orderData.m_status;
         if (orderStatus == OrderStatus.NEW) {
-            console("onSecTimer: order not yet submitted");
+            console("onTimer: order not yet submitted");
             return false;
         }
         if (orderStatus == OrderStatus.FILLED) {
-            console("onSecTimer: order is already filled");
+            console("onTimer: order is already filled");
             m_state = State.done;
             return true;
         }
@@ -86,19 +86,19 @@ class OrderWatcher {
             boolean move = false;
             long outOfSpreadOld = m_outOfTopSpreadStamp.getPassedMillis();
             if (outOfSpreadOld > MAX_LIVE_OUT_OF_SPREAD) {
-                console("onSecTimer: order out of top spread already " + outOfSpreadOld + " ms. moving...");
+                console("onTimer: order out of top spread already " + outOfSpreadOld + " ms. moving...");
                 move = true;
                 priceStepRate = 0.05;
             } else {
                 long onTopSpreadWithOthersOld = m_onTopSpreadWithOthersStamp.getPassedMillis();
                 if (onTopSpreadWithOthersOld > MAX_LIVE_ON_SPREAD_WITH_OTHERS) {
-                    console("onSecTimer: order on top spread with others already " + onTopSpreadWithOthersOld + " ms. moving...");
+                    console("onTimer: order on top spread with others already " + onTopSpreadWithOthersOld + " ms. moving...");
                     move = true;
                     priceStepRate = 0.1;
                 } else {
                     long onTopSpreadOld = m_onTopSpreadAloneStamp.getPassedMillis();
                     if (onTopSpreadOld > MAX_LIVE_ON_SPREAD) {
-                        console("onSecTimer: order on top spread alone already " + onTopSpreadOld + " ms. moving...");
+                        console("onTimer: order on top spread alone already " + onTopSpreadOld + " ms. moving...");
                         move = true;
                         priceStepRate = 0.15;
                     }
@@ -108,47 +108,47 @@ class OrderWatcher {
             if (move) {
                 double minOrderToCreate = m_exchPairData.m_minOrderToCreate.m_value;
                 double remained = m_orderData.remained();
-                console(" remained=" + remained + "; minOrderToCreate=" + minOrderToCreate);
-                if (remained < minOrderToCreate) {
+                console(" remained=" + Utils.format8(remained) + "; minOrderToCreate=" + minOrderToCreate);
+                if (remained >= minOrderToCreate) {
+                    OrderBook orderBook = m_exchPairData.getOrderBook();
+                    OrderBook.Spread topSpread = orderBook.getTopSpread();
+                    double orderPrice = m_orderData.m_price;
+                    double minPriceStep = m_exchPairData.m_minPriceStep;
+                    console(" orderBook.topSpread=" + topSpread + "; orderPrice=" + orderPrice + "; minPriceStep=" + Utils.format8(minPriceStep));
+                    OrderSide side = m_orderData.m_side;
+                    boolean isBuy = side.isBuy();
+
+                    double askPrice = topSpread.m_askEntry.m_price;
+                    double bidPrice = topSpread.m_bidEntry.m_price;
+                    double topSpreadDiff = askPrice - bidPrice;
+                    double priceStep = topSpreadDiff * priceStepRate;
+                    console(" topSpreadDiff=" + Utils.format8(topSpreadDiff) + ";priceStepRate=" + priceStepRate + ";  priceStep=" + Utils.format8(priceStep));
+                    if (priceStep < minPriceStep) {
+                        priceStep = minPriceStep;
+                        console("  priceStep fixed to minPriceStep=" + minPriceStep);
+                    }
+
+                    double price = isBuy ? bidPrice + priceStep : askPrice - priceStep;
+                    console("   price=" + Utils.format8(price) + ";  spread=" + topSpread);
+
+                    String replaceOrderId = m_orderData.m_orderId;
+                    OrderData orderData = m_orderData.copyForReplace();
+                    orderData.m_price = price;
+                    console("    submitOrderReplace: " + orderData);
+                    if (Tre.SEND_REPLACE) {
+                        orderData.addOrderListener(m_orderListener);
+                        m_orderData = orderData;
+                        m_exchange.submitOrderReplace(replaceOrderId, orderData);
+                        m_outOfTopSpreadStamp.reset(); // reset
+                        m_onTopSpreadAloneStamp.reset(); // reset
+                        m_onTopSpreadWithOthersStamp.reset(); // reset
+                    }
+                } else {
                     console("  remained qty is too low for move order" );
-                }
-
-                OrderBook orderBook = m_exchPairData.getOrderBook();
-                OrderBook.Spread topSpread = orderBook.getTopSpread();
-                double orderPrice = m_orderData.m_price;
-                double minPriceStep = m_exchPairData.m_minPriceStep;
-                console(" orderBook.topSpread=" + topSpread + "; orderPrice=" + orderPrice + "; minPriceStep=" + Utils.format8(minPriceStep));
-                OrderSide side = m_orderData.m_side;
-                boolean isBuy = side.isBuy();
-
-                double askPrice = topSpread.m_askEntry.m_price;
-                double bidPrice = topSpread.m_bidEntry.m_price;
-                double topSpreadDiff = askPrice - bidPrice;
-                double priceStep = topSpreadDiff * priceStepRate;
-                console(" topSpreadDiff=" + Utils.format8(topSpreadDiff) + ";priceStepRate=" + priceStepRate + ";  priceStep=" + Utils.format8(priceStep));
-                if (priceStep < minPriceStep) {
-                    priceStep = minPriceStep;
-                    console("  priceStep fixed to minPriceStep=" + minPriceStep);
-                }
-
-                double price = isBuy ? bidPrice + priceStep : askPrice - priceStep;
-                console("   price=" + price + ";  spread=" + topSpread);
-
-                String replaceOrderId = m_orderData.m_orderId;
-                OrderData orderData = m_orderData.copyForReplace();
-                orderData.m_price = price;
-                console("    submitOrderReplace: " + orderData);
-                if (Tre.SEND_REPLACE) {
-                    orderData.addOrderListener(m_orderListener);
-                    m_orderData = orderData;
-                    m_exchange.submitOrderReplace(replaceOrderId, orderData);
-                    m_outOfTopSpreadStamp.reset(); // reset
-                    m_onTopSpreadAloneStamp.reset(); // reset
-                    m_onTopSpreadWithOthersStamp.reset(); // reset
                 }
             }
         } catch (Exception e) {
-            String msg = "OrderWatcher.onSecTimer() error: " + e;
+            String msg = "OrderWatcher.onTimer() error: " + e;
             console(msg);
             err(msg, e);
         }
@@ -181,13 +181,13 @@ class OrderWatcher {
             double sidePrice = entry.m_price;
             double delta = sidePrice - orderPrice;
             double minPriceStep = m_exchPairData.m_minPriceStep;
-            console("  side price " + (isBuy ? "buy" : "ask") + "Price=" + Utils.format8(sidePrice)
+            log("  side price " + (isBuy ? "buy" : "ask") + "Price=" + Utils.format8(sidePrice)
                     + "; delta=" + Utils.format12(delta) + "; minPriceStep=" + Utils.format8(minPriceStep));
 
             if (Math.abs(delta) < (minPriceStep / 2)) {
                 double entrySize = entry.m_size;
                 double orderRemained = m_orderData.remained();
-                console("  order is on spread side. entrySize=" + entrySize + "; orderRemained=" + orderRemained);
+                console("  order is on spread side. entrySize=" + Utils.format8(entrySize) + "; orderRemained=" + Utils.format8(orderRemained));
                 if (entrySize > orderRemained) {
                     m_onTopSpreadWithOthersStamp.startIfNeeded();
                     m_onTopSpreadAloneStamp.reset();
