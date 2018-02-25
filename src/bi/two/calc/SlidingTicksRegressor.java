4f968e6449d8e6ff14710b331bf2641417e2e352
papikcar <papikcar@gmail.com>
2018-02-25 04:27:48 +0200

    +ticksPool

--- src/bi/two/calc/SlidingTicksRegressor.java
+++ src/bi/two/calc/SlidingTicksRegressor.java
@@ -8,6 +8,9 @@ import bi.two.ts.ITimesSeriesData;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 
 public class SlidingTicksRegressor extends BaseTimesSeriesData<ITickData> {
+    private static final int TICK_POOL_SIZE = 10;
+
+    private final boolean m_collectValues;
     private final SimpleRegression m_simpleRegression = new SimpleRegression(true);
     private final BarSplitter m_splitter;
     private long m_firstTimestamp;
@@ -15,8 +18,15 @@ public class SlidingTicksRegressor extends BaseTimesSeriesData<ITickData> {
     private boolean m_initialized;
     private boolean m_filled;
     private TickData m_lastTick;
+    private TickData[] m_ticksPool = new TickData[TICK_POOL_SIZE];
+    private int m_ticksPoolPosition;
 
     public SlidingTicksRegressor(ITimesSeriesData<ITickData> tsd, long period) {
+        this(tsd, period, true);
+    }
+
+    public SlidingTicksRegressor(ITimesSeriesData tsd, long period, boolean collectValues) {
+        m_collectValues = collectValues;
         m_splitter = new BarSplitter(tsd, 1, period);
         setParent(m_splitter);
     }
@@ -41,7 +51,7 @@ public class SlidingTicksRegressor extends BaseTimesSeriesData<ITickData> {
                         float price = tickData.getClosePrice();
                         long timestamp = tickData.getTimestamp();
                         m_simpleRegression.removeData(timestamp - m_firstTimestamp, price);
-                        m_lastTick = null;
+                        m_lastTick = null; // mark as dirty
                     }
                 });
                 m_initialized = true;
@@ -65,10 +75,23 @@ public class SlidingTicksRegressor extends BaseTimesSeriesData<ITickData> {
 
     @Override public ITickData getLatestTick() {
         if (m_filled) {
-            if (m_lastTick == null) {
+            if (m_lastTick == null) { // dirty ?
                 if (m_simpleRegression.getN() >= 2) { // enough ticks
                     double regression = m_simpleRegression.predict(m_lastTimestamp - m_firstTimestamp);
-                    m_lastTick = new TickData(m_lastTimestamp, (float) regression);
+
+                    if (m_collectValues) {
+                        m_lastTick = new TickData(m_lastTimestamp, (float) regression);
+                    } else { // use object from pool to minimize allocations
+                        TickData fromPool = m_ticksPool[m_ticksPoolPosition];
+                        if (fromPool == null) {
+                            fromPool = new TickData(m_lastTimestamp, (float) regression);
+                            m_ticksPool[m_ticksPoolPosition] = fromPool;
+                        } else {
+                            fromPool.init(m_lastTimestamp, (float) regression);
+                        }
+                        m_lastTick = fromPool;
+                        m_ticksPoolPosition = (m_ticksPoolPosition + 1) % TICK_POOL_SIZE;
+                    }
                 }
             }
             return m_lastTick;
