f8651de52fbb02f43641fa18dce06b599c2266c7
papikcar <papikcar@gmail.com>
2018-03-14 11:46:28 +0200

    +Ummar3Algo

--- src/bi/two/algo/Algo.java
+++ src/bi/two/algo/Algo.java
@@ -5,39 +5,17 @@ import bi.two.ts.ITimesSeriesData;
 import bi.two.util.MapConfig;
 
 public enum Algo {
-    regression {
-        @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) {
-            return new RegressionAlgo(algoConfig, parent);
-        }
-    },
-    emaTrend {
-        @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) {
-            return new EmaTrendAlgo(algoConfig, parent);
-        }
-    },
+    regression { @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) { return new RegressionAlgo(algoConfig, parent); } },
+    emaTrend { @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) { return new EmaTrendAlgo(algoConfig, parent); } },
     mmar {
-        @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) {
-            return new MmarAlgo(algoConfig, parent);
-        }
+        @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) { return new MmarAlgo(algoConfig, parent); }
         @Override public void resetIterationCache() {
             MmarAlgo.resetIterationCache();
-        }
-    },
-    mmar3 {
-        @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) {
-            return new Mmar3Algo(algoConfig, parent);
-        }
-    },
-    ummar {
-        @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) {
-            return new UmmarAlgo(algoConfig, parent);
-        }
-    },
-    ummar2 {
-        @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) {
-            return new Ummar2Algo(algoConfig, parent);
-        }
-    },
+        } },
+    mmar3 { @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) { return new Mmar3Algo(algoConfig, parent); } },
+    ummar { @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) { return new UmmarAlgo(algoConfig, parent); } },
+    ummar2 { @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) { return new Ummar2Algo(algoConfig, parent); } },
+    ummar3 { @Override public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) { return new Ummar3Algo(algoConfig, parent); } },
     ;
 
     public BaseAlgo createAlgo(MapConfig algoConfig, ITimesSeriesData parent) {
