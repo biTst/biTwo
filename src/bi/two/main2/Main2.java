8bb3233ee2f262c9b59870e9449c2d3bf49bd7ec
papikcar <papikcar@gmail.com>
2018-09-07 06:19:31 +0300

    -

--- src/bi/two/main2/Main2.java
+++ src/bi/two/main2/Main2.java
@@ -25,7 +25,8 @@ public class Main2 extends Thread {
     private static final String CONFIG = "cfg\\main2.properties";
 //    private static final long PRELOAD_PERIOD = TimeUnit.MINUTES.toMillis(50);
 //    private static final long PRELOAD_PERIOD = TimeUnit.HOURS.toMillis(24);
-    private static final long PRELOAD_PERIOD = TimeUnit.DAYS.toMillis(3);
+//    private static final long PRELOAD_PERIOD = TimeUnit.DAYS.toMillis(3);
+    private static final long PRELOAD_PERIOD = TimeUnit.DAYS.toMillis(10);
 
     private Exchange m_exchange;
     private Pair m_pair;
@@ -62,7 +63,7 @@ public class Main2 extends Thread {
             boolean collectTicks = true;
             m_ticksTs = collectTicks
                     ? new TicksTimesSeriesData<TickData>(null)
-                    : new NoTicksTimesSeriesData<TickData>(null); // : new TicksTimesSeriesDataToDelete(collectTicks);
+                    : new NoTicksTimesSeriesData<TickData>(null);
             if (!collectTicks) { // add initial tick to update
                 m_ticksTs.addOlderTick(new TickData());
             }
